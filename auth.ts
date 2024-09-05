f6227fbb32bb05941cad315d1799b83d75757cf6
Srijan <chaturchacharost@gmail.com>
2024-09-05 10:55:19 +0530

    added auth

--- auth.ts
+++ auth.ts
@@ -7,6 +7,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
   callbacks:{
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      console.log(auth)
+      return !!auth
+    }
+  ,
     signIn({account,profile,user}){
       console.log({account,profile,user});
       console.log(user.email?.split('@'))
