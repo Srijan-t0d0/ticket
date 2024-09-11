d0682300f5aed838e1daf426180d528ccdaf4eec
Srijan <chaturchacharost@gmail.com>
2024-09-12 00:35:57 +0530

    refactor

---
+++ app/(protected)/bookticket/page.tsx
@@ -0,0 +1,15 @@
+import { getBookedSeats, getBookingData } from "@/actions";
+import { auth } from "@/auth";
+import { Seats } from "@/components/seats";
+import { prisma } from "@/prisma";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const session = await auth();
+  const { booking } = await getBookingData();
+  if (booking) {
+    redirect("/print-ticket");
+  }
+  const bookings = await getBookedSeats();
+  return <Seats bookings={bookings} />;
+}
---
+++ app/(protected)/print-ticket/page.tsx
@@ -0,0 +1,10 @@
+import PrintTicket from "@/components/printTicket";
+import { Ticket } from "@/components/ticket";
+
+export default function Page() {
+  return (
+    <PrintTicket>
+      <Ticket />
+    </PrintTicket>
+  );
+}
