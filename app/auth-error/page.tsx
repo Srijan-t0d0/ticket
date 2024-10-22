ce89feab5e0e9b4279ed88f53cb9805bcb60badf
Srijan-t0d0 <chaturchacharost@gmail.com>
2024-10-22 21:18:00 +0530

    fixig auth-error

--- app/auth-error/page.tsx
+++ app/auth-error/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { signIn } from "@/actions";
-import { signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/dist/server/api-utils";
 import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function ErrorPage() {
+  const router = useRouter();
   useEffect(() => {
     signOut();
   });
+
+  const session = useSession();
+  if (session?.data?.user) return router.push("/book-ticket");
   return (
     <div className="h-screen bg-slate-900 w-screen flex justify-center items-center">
       <div className="bg-black p-5 rounded-lg flex flex-col justify-center items-center gap-4">
