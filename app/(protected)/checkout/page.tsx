6e93af8407d43b0c4fa3a48d3991f8baf0be5b91
Srijan-t0d0 <chaturchacharost@gmail.com>
2024-10-23 19:28:18 +0530

    fuck razorpay (not using razorpay anymore)

--- app/(protected)/checkout/page.tsx
+++ app/(protected)/checkout/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { getBookedSeats } from "@/actions";
+import { createBooking, getBookedSeats } from "@/actions";
 import { useSeatContext } from "@/components/SeatProvider";
 
 import { redirect, useRouter, useSearchParams } from "next/navigation";
@@ -16,49 +16,19 @@ export default function Page() {
 }
 
 function Checkout({ selectedSeats }: { selectedSeats: number[] }) {
-  const createOrderId = async () => {
-    try {
-      const response = await fetch("/api/order", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          amount: 40 * 100,
-          seat: selectedSeats,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-      return data.orderId;
-    } catch (error) {
-      console.error("There was a problem with your fetch operation:", error);
-    }
-  };
-
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
   const handleClick = async () => {
-    const orderId = await createOrderId();
-    const options = {
-      key: process.env.NEXT_PUBLIC_RP_key_id, // Replace with your Razorpay key_id
-      amount: "4000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-      currency: "INR",
-
-      order_id: orderId, // This is the order_id created in the backend
-      callback_url: ` ${process.env.NEXT_PUBLIC_AUTH_URL}/api/payment-success`, // Your success URL
-      redirect: true,
-
-      theme: {
-        color: "#F37254",
-      },
-    };
-    //@ts-expect-error
-    const rzp = new window.Razorpay(options);
-    rzp.open();
+    setLoading(true);
+    const res = await createBooking([selectedSeats[0].toString()]);
+    if (res.error) {
+      alert(`Seat ${selectedSeats[0]} is already booked`);
+      router.push("booktiket");
+    }
+    router.push("/print-ticket");
+    setLoading(false);
   };
+
   return (
     <Suspense>
       <div className="h-screen w-full flex flex-col items-center justify-center">
@@ -66,12 +36,12 @@ function Checkout({ selectedSeats }: { selectedSeats: number[] }) {
           <h1 className="text-4xl font-bold my-2"> Checkout</h1>
           <hr></hr>
           <p>Selected Seat: {selectedSeats?.toString()}</p>
-          <p>Amount :&#x20b9; {selectedSeats?.length * 40}</p>
           <button
+            disabled={loading}
             className="bg-teal-600 p-2 m-2 rounded-lg hover:bg-teal-700 active:bg-teal-900"
             onClick={handleClick}
           >
-            Confirm
+            {loading ? "Loading...." : "Checkout"}
           </button>
         </div>
       </div>
